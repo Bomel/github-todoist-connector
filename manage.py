a945d22f19a53d48f29ea03d1c738f3f22e9bfd9
Keiwan Jamaly <keiwanjamaly@gmail.com>
2021-04-10 00:34:02 +0200

    added one worker only

--- manage.py
+++ manage.py
@@ -7,10 +7,19 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    main()
-    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+    global in_use
+    if in_use:
+        print("Process in use!")
+        return json.dumps({'success': True, 'in_use': True}), 200, {'ContentType': 'application/json'}
+    else:
+        in_use = True
+        main()
+        in_use = False
+        return json.dumps({'success': True, 'in_use': False}), 200, {'ContentType': 'application/json'}
 
 
 if __name__ == "__main__":
+    global in_use
+    in_use = False
     from waitress import serve
     serve(app, host="0.0.0.0", port=5000)
