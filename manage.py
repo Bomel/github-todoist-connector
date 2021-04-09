88caceef14a116f5902c1c62e2ba613f2c710a45
Keiwan Jamaly <keiwanjamaly@gmail.com>
2021-04-09 23:31:52 +0200

    added dockerfile

--- manage.py
+++ manage.py
@@ -1,14 +1,17 @@
 import json
 from flask import Flask
+from sync_tasks import main
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return json.dumps({'name': 'alice',
-                       'email': '[email protected]'})
+    main()
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
 
 if __name__ == "__main__":
-    app.run()
+    from waitress import serve
+    print("Server is listening to http://0.0.0.0:5000")
+    serve(app, host="0.0.0.0", port=5000)
